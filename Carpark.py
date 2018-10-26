b12b51a0083e78e88468b0b8c4eddd956edcbcfd
Khairul Slt <khairulslt@hotmail.sg>
2018-10-26 10:53:19 +0800

    removed a route, refactored css/html code

--- Carpark.py
+++ Carpark.py
@@ -5,42 +5,46 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def indexPage():
+def index_page():
 	return render_template("index.html")
 
-@app.route('/address',methods = ['POST', 'GET'])
-def address1():
-	if request.method == 'POST':
-		desired_CP_Address = request.form["address"]
 
-		response = requests.get("https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c&q=" + desired_CP_Address)
+'''
+address route receives POST data from index.html form and "plugs" that into first API
+renders list of addresses returned from API
+'''
+@app.route('/address',methods = ['POST', 'GET'])
+def address_listings():
+	if request.method == 'POST':
+		cp_address = request.form["address"] # form data
+
+		response = requests.get("https://data.gov.sg/api/action/datastore_search?resource_id=139a3035-e624-4f56-b63f-89ae28d4ae4c&q=" + cp_address)
 		r = response.json()
 
-		CP_Info = r['result']['records']
-		return render_template("address1.html", text=CP_Info)
+		cp_info = r['result']['records']
+		return render_template("addresses.html", text=cp_info)
 
 
-@app.route('/lotfinder',methods = ['POST', 'GET'])
-def car1():
-	if request.method == 'GET':
-		return render_template("car1.html")
-
-# Second API
+'''
+carpark route receives POST data from addresses.html form and "plugs" that into second API
+renders carpark availability from API
+'''
 @app.route('/carpark',methods = ['POST', 'GET'])
-def car2():
+def carpark_availability():
 	if request.method == 'POST':
-		desired_CP_Number = request.form["Carpark"] 
+		cp_number = request.form["Carpark"] 
 
-		carparkApi = requests.get("https://api.data.gov.sg/v1/transport/carpark-availability")
-		carparkJson = carparkApi.json()
-		CP_Data = carparkJson['items'][0]["carpark_data"]
-		for x in CP_Data: 								
-			if x["carpark_number"] == desired_CP_Number:
-				CP_lots = x["carpark_info"]
-				raw_timeStamp = x['update_datetime']  # 2018-10-08T18:06:06 
-				timeStamp = raw_timeStamp[-8:] + '  /' + raw_timeStamp[:-9]
-				return render_template("car2.html", text=CP_lots, data=desired_CP_Number, time=timeStamp) 
-		return render_template("car2.html", text="", data=desired_CP_Number)
+		carpark_api = requests.get("https://api.data.gov.sg/v1/transport/carpark-availability")
+		carpark_json = carpark_api.json()
+		cp_data = carpark_json['items'][0]["carpark_data"]
+
+		for x in cp_data: 								
+			if x["carpark_number"] == cp_number:
+				cp_lots = x["carpark_info"]
+				raw_time = x['update_datetime']  # 2018-10-08T18:06:06 
+				timestamp = raw_time[-8:] + '  /' + raw_time[:-9]
+				return render_template("carparks.html", text=cp_lots, data=cp_number, time=timestamp) 
+		return render_template("carparks.html", text="", data=cp_number) # else error handling
 		
 if __name__ == '__main__':
     app.run(debug=True)
